6b091600765d0162e5783bbb35dbda0535540b2b
fjadbeth <74471478+fjadbeth@users.noreply.github.com>
2022-02-13 16:55:23 -0600

    Starting to tidy some data

--- Test.R
+++ Test.R
@@ -1,4 +1,43 @@
-my_data <- read.csv("IPEDS_data.csv")
+library(tidyverse)
 
+my_data <- read.csv("IPEDS_data.csv") %>%
+  select(2, 5, 7:8, 22:24, 27:34, 65, 70:73, 75, 78, 86, 111, 124, 126, 128, 131, 134)
+
+my_data <- my_data %>% 
+  rename(High.Degree = Highest.degree.offered,
+         Longitude = Longitude.location.of.institution,
+         Latitude = Latitude.location.of.institution,
+         Applicants = Applicants.total,
+         Admissions = Admissions.total,
+         Enrolled = Enrolled.total,
+         SAT.Reading.25 = SAT.Critical.Reading.25th.percentile.score,
+         SAT.Reading.75 = SAT.Critical.Reading.75th.percentile.score,
+         SAT.Math.25 = SAT.Math.25th.percentile.score,
+         SAT.Math.75 = SAT.Math.75th.percentile.score,
+         SAT.Writing.25 = SAT.Writing.25th.percentile.score,
+         SAT.Writing.75 = SAT.Writing.75th.percentile.score,
+         ACT.25 = ACT.Composite.25th.percentile.score,
+         ACT.75 = ACT.Composite.75th.percentile.score,
+         Percent.Admitted = Percent.admitted...total,
+         Tuition = Tuition.and.fees..2013.14,
+         Tuition.Instate = Total.price.for.in.state.students.living.on.campus.2013.14,
+         Tuition.Outstate = Total.price.for.out.of.state.students.living.on.campus.2013.14,
+         State = State.abbreviation,
+         Region = Geographic.region,
+         Control = Control.of.institution,
+         TEnrollemend = Undergraduate.enrollment,
+         Percent.Women = Percent.of.undergraduate.enrollment.that.are.women,
+         Pecent.Instate = Percent.of.first.time.undergraduates...in.state,
+         Percent.Outstate = Percent.of.first.time.undergraduates...out.of.state,
+         Percent.Foreign = Percent.of.first.time.undergraduates...foreign.countries,
+         GradRate.4yr = Graduation.rate...Bachelor.degree.within.4.years..total,
+         Percent.Finan.Aid = Percent.of.freshmen.receiving.any.financial.aid
+         )
 
 # What to filter: Name, 
+filter(my_data, my_data$SAT.Reading.75 > 780 & my_data$Percent.Women > 40)
+filter(my_data, my_data$ACT.25 > 33 | my_data$SAT.Math.75 > 760 & my_data$Percent.Admitted > 20)
+filter(my_data, my_data$Pecent.Instate < 40, my_data$Percent.Foreign > 15)
+filter(my_data, my_data$Percent.Finan.Aid > 60, my_data$TEnrollemend < 1000, my_data$Control == "Public")
+
+hist(my_data$Percent.Admitted)
